b4dc93021f05baaf1ec5cfac3dbe21900619bbe8
Annamikya <nigamannamikya@gmail.com>
2026-07-31 13:44:43 +0530

    Fix quiz flashcards notes and chat

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-const { askQuestion } = require("../controllers/chatController");
-const { generalChat } = require("../controllers/generalChatController");
+const authMiddleware = require(
+  "../middleware/authMiddleware"
+);
+
+const {
+  askQuestion,
+} = require("../controllers/chatController");
+
+const {
+  generalChat,
+} = require("../controllers/generalChatController");
 
 // General AI Chat
 router.post("/general", generalChat);
 
-// Chat with Uploaded PDF
-router.post("/ask", askQuestion);
+// Chat with uploaded PDF
+router.post(
+  "/ask",
+  authMiddleware,
+  askQuestion
+);
 
 module.exports = router;
